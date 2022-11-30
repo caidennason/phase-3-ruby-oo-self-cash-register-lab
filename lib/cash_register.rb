787153dcb57a851298797ffdcd8f216ccf9f9a4b
caiden nason <caidenbnason@gmail.com>
2022-11-30 13:40:31 -0500

    commit tree 290a29af30614673e3e745b1bf94597448e8f82c

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,48 @@
+require 'pry'
 
+class CashRegister
+
+    attr_accessor :discount, :total, :quantity, :title, :arr, :price_arr
+
+    def initialize(discount = 0)
+        @discount = discount
+        @total = 0
+        @arr = [] #why initialize it here??
+        @price_arr = []
+    end
+
+    def total
+        @total
+    end
+
+    def add_item(title, price, quantity = 1)
+        @total += price * quantity
+        @quantity = quantity
+        @quantity.times do 
+            @arr.push(title)
+            @price_arr.push(title, price)
+        end
+    end
+
+    def apply_discount
+        self.total = self.total - @discount * 10
+        if self.discount > 0
+        "After the discount, the total comes to $#{self.total}."
+        else
+        "There is no discount to apply."
+        end
+    end
+
+    def items
+        @arr
+    end
+
+    def void_last_transaction
+       @total -= @price_arr[-1]
+       if @arr.length == @quantity
+        @total = 0
+       end
+    end
+
+#binding.pry
+end
